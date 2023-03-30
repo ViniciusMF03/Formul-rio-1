b4c27874b2b4a6461631d2fe5196b41d49ce6c15
ViniciusMF03 <viniciusmacedo581@gmail.com>
2023-03-30 12:53:39 -0400

    commit inicial

---
+++ script.js
@@ -0,0 +1,13 @@
+
+const form = document.getElementById("form");
+const username = document.getElementById("username")
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const passwordConfirmation = document.getElementById("password-confirmation")
+
+
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    alert("CADASTRADO COM SUCESSO")
+})
